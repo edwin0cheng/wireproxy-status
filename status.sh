2745f50a228d5fc8fbb83cc0c888fde954e7f894
Edwin Cheng <edwin0cheng@gmail.com>
2025-06-06 08:55:15 +0800

    Add test connection from the proxy server.

--- status.sh
+++ status.sh
@@ -6,11 +6,14 @@ config=${name##*/}
 if [[ -z "$config" ]]; then
 	echo "off"
 else
+	# Test the connection:
+	conn="$(curl --silent --head --fail --output /dev/null --socks5-hostname socks5://127.0.0.1:25344 https://www.github.com || echo ' off')"
 	loc=${config##*-}
+	
 	if [[ "$loc" == "config" ]]; then
-		echo "default"
+		echo "default$conn"
 	else
-		echo "$loc"
+		echo "$loc$conn"
 	fi
 fi
 
