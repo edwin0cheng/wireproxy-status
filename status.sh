4100e994dbc9affa3ba4fcfe02179ee5dcaac320
Edwin Cheng <edwin0cheng@gmail.com>
2025-06-20 14:35:58 +0800

    Change to daemon style

--- status.sh
+++ status.sh
@@ -1,44 +1,37 @@
 #!/bin/bash
 
-function write_connection() {
-	local conn="$(curl --max-time 5 --silent --head --fail --output /dev/null --socks5-hostname socks5://127.0.0.1:25344 https://www.github.com || echo ' off')"
-	local file="/tmp/wireproxy-connection"
-	echo "$conn" > "$file"
-}
-
-# If first argument is "write", write the connection status to a file.
-if [[ "$1" == "write" ]]; then
-	write_connection
-	exit 0
-fi
-
 function read_connection() {
-	local file="/tmp/wireproxy-connection"
-	if [[ -f "$file" ]]; then
-		cat "$file"
+	local name=$(docker ps --no-trunc --format "json" | jq -r 'select(.Names == "wireproxy").Command' | sed 's/^"\(.*\)"$/\1/')
+
+	local config=${name##*/}
+
+	if [[ -z "$config" ]]; then
+		echo "off"
 	else
-		echo " .."
+		# Test the connection:
+		local conn="$(curl --max-time 5 --silent --head --fail --output /dev/null --socks5-hostname socks5://127.0.0.1:25344 https://www.github.com || echo ' off')"
+
+		loc=${config##*-}
+		
+		if [[ "$loc" == "config" ]]; then
+			echo "default$conn"
+		else
+			echo "$loc$conn"
+		fi
 	fi
 }
 
-name=$(docker ps --no-trunc --format "json" | jq -r 'select(.Names == "wireproxy").Command' | sed 's/^"\(.*\)"$/\1/')
+echo ".."
 
-config=${name##*/}
+while [[ true ]]; do
+	SECONDS=0
+	read_connection
+	time_elapsed=$SECONDS
 
-if [[ -z "$config" ]]; then
-	echo "off"
-else
-	# Test the connection:
-	conn=$(read_connection)
-	# write the connection using this script
-	setsid --fork ${0} write
-
-	loc=${config##*-}
-	
-	if [[ "$loc" == "config" ]]; then
-		echo "default$conn"
-	else
-		echo "$loc$conn"
+	# if time elapsed is less than 5 seconds, sleep for the remaining time
+	if [[ $time_elapsed -lt 5 ]]; then
+		sleep $((5 - time_elapsed))
 	fi
-fi
+done
+
 
